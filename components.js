f7a87cae3153b449c1a4b8d5af15309f2bc4e884
88potatoes <94770511+88potatoes@users.noreply.github.com>
2023-12-18 23:38:52 +1100

    header and footer links

--- components.js
+++ components.js
@@ -1,7 +1,7 @@
 class Header extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
-        <nav class="container-fluid h-5 bg-info">
+        <nav class="container-fluid h-5 bg-info" style="height: 5rem;">
             <div class="container h-100 bg-warning d-flex align-items-center justify-content-center">
                 <div class="row w-100">
                     <div class="col-md-8 text-white">
@@ -9,9 +9,9 @@ class Header extends HTMLElement {
                     </div>
                     <div class="col">
                         <div class="row align-items-center h-100 text-center">
-                            <div class="col-4">About</div>
-                            <div class="col-4">Contact</div>
-                            <div class="col-4">Services</div>
+                            <div class="col-4"><a href="./about.html">About</a></div>
+                            <div class="col-4"><a href="./contact.html">Contact</a></div>
+                            <div class="col-4"><a href="./servicelist.html">Services</a></div>
                         </div>
                     </div>
                 </div>
@@ -30,22 +30,22 @@ class Footer extends HTMLElement {
                 <div class="row">
                     <div class="col">
                         <div class="bg-secondary text-white p-2">
-                            Content Policy
+                            <a href="./base.html">Content Policy</a>
                         </div>
                         <div class="bg-secondary text-white p-2">
-                            Privacy Statement
+                            <a href="./base.html">Privacy Statement</a>
                         </div>
                         <div class="bg-secondary text-white p-2">
-                            Cancellation Policy
+                            <a href="./base.html">Cancellation Policy</a>
                         </div>
                     </div>
                 </div>
                 <div class="row align-items-center">
                     <div class="col-4 ms-auto h-100 order-sm-1 d-flex align-items-center justify-content-center">
                         <ul class="d-flex w-100 justify-content-around h-100 align-items-center list-unstyled">
-                            <li><i class="bi bi-facebook h3"></i></li>
-                            <li><i class="bi bi-twitter h3"></i></li>
-                            <li><i class="bi bi-instagram h3"></i></li>
+                            <li><a href="https://www.facebook.com/"><i class="bi bi-facebook h3"></i></a></li>
+                            <li><a href="https://twitter.com/home?lang=en"><i class="bi bi-twitter h3"></i></a></li>
+                            <li><a href="https://www.instagram.com/"><i class="bi bi-instagram h3"></i></a></li>
                         </ul>
                     </div>
                     <div class="col-sm-8">
