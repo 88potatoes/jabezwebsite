d9ff19f32fbdcb8aef5df4905ec82b2c93309ff9
88potatoes <94770511+88potatoes@users.noreply.github.com>
2023-12-19 16:57:39 +1100

    improve readme + improve styling on header and footer

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 website link: https://88potatoes.github.io/jabezwebsite/base.html
+
+to start dev, clone repo
+
+then run
+```
+npm install
+npx sass custom.scss assets/bs_css.css --watch
+```
+
+then you can run on liveserver
--- components.js
+++ components.js
@@ -1,16 +1,16 @@
 class Header extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
-        <nav class="navbar navbar-expand-sm container-fluid bg-info">
-            <div class="container h-100 bg-warning d-flex align-items-center justify-content-center">
+        <nav class="navbar navbar-expand-sm container-fluid bg-primary">
+            <div class="container h-100 d-flex align-items-center justify-content-center">
                 <div class="row w-100">
                     <div class="col col-sm-8 text-white h1 mb-0">
                         JY Health
                     </div>
                     <nav class="justify-content-end text-center col col-sm-4 collapse navbar-collapse align-items-center" id="navElements">
-                        <a href="./about.html" class="p-2 nav-link">About</a>
-                        <a href="./contact.html" class="p-2 nav-link">Contact</a>
-                        <a href="./servicelist.html" class="p-2 nav-link">Services</a>
+                        <a href="./about.html" class="p-2 nav-link text-white">About</a>
+                        <a href="./contact.html" class="p-2 nav-link text-white">Contact</a>
+                        <a href="./servicelist.html" class="p-2 nav-link text-white">Services</a>
                     </nav>
                     <button class="navbar-toggler border-0" type="button" data-bs-toggle="collapse" data-bs-target="#navElements" aria-expanded="false" aria-controls="navElements" aria-expanded="false" aria-label="Toggle Navigation">
                         <span class="navbar-toggler-icon"></span>
@@ -27,17 +27,17 @@ customElements.define("header-component", Header);
 class Footer extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
-        <div class="container-fluid bg-info">
-            <div class="container bg-warning h-100">
+        <div class="container-fluid bg-primary">
+            <div class="container h-100">
                 <div class="row">
                     <div class="col">
-                        <div class="bg-secondary text-white p-2">
+                        <div class="text-white p-2">
                             <a href="./base.html" class="nav-link">Content Policy</a>
                         </div>
-                        <div class="bg-secondary text-white p-2">
+                        <div class="text-white p-2">
                             <a href="./base.html" class="nav-link">Privacy Statement</a>
                         </div>
-                        <div class="bg-secondary text-white p-2">
+                        <div class="text-white p-2">
                             <a href="./base.html" class="nav-link">Cancellation Policy</a>
                         </div>
                     </div>
@@ -45,13 +45,13 @@ class Footer extends HTMLElement {
                 <div class="row align-items-center">
                     <div class="col-4 ms-auto h-100 order-sm-1 d-flex align-items-center justify-content-center">
                         <ul class="d-flex w-100 justify-content-around h-100 align-items-center list-unstyled">
-                            <li><a href="https://www.facebook.com/" class="nav-link"><i class="bi bi-facebook h3 m-0"></i></a></li>
-                            <li><a href="https://twitter.com/home?lang=en" class="nav-link"><i class="bi bi-twitter h3 m-0"></i></a></li>
-                            <li><a href="https://www.instagram.com/" class="nav-link"><i class="bi bi-instagram h3 m-0"></i></a></li>
+                            <li><a href="https://www.facebook.com/" class="nav-link"><i class="bi bi-facebook h3 m-0 text-white"></i></a></li>
+                            <li><a href="https://twitter.com/home?lang=en" class="nav-link"><i class="bi bi-twitter h3 m-0 text-white"></i></a></li>
+                            <li><a href="https://www.instagram.com/" class="nav-link"><i class="bi bi-instagram h3 m-0 text-white"></i></a></li>
                         </ul>
                     </div>
                     <div class="col-sm-8">
-                        <div class="bg-primary text-white p-2 m-2">
+                        <div class=" text-white p-2 m-2 border-top border-white">
                         © JY Health 2023
                         </div>
                     </div>
